61d34ae321388e0f63c1033f2c199a6c3c4e1406
Daniel Radetsky <dradetsky@gmail.com>
2015-07-07 18:29:41 -0700

    can probably delete whole file, actually

--- System/Posix/ARX/BlazeIsString.hs
+++ System/Posix/ARX/BlazeIsString.hs
@@ -7,6 +7,26 @@ import qualified Blaze.ByteString.Builder as Blaze
 import qualified Blaze.ByteString.Builder.Char8 as Blaze
 
 
-instance IsString Blaze.Builder where
-  fromString                 =  Blaze.fromString
+-- We can probably comment out the below because:
+--
+-- Blaze.ByteString.Builder.Char8
+-- ------------------------------
+--
+-- -- | /O(n)/. Serialize the lower 8-bits of all characters of a string
+-- fromString :: String -> Builder
+-- fromString = P.primMapListFixed P.char8
+--
+-- Data.ByteString.Builder
+-- -----------------------
+--
+-- instance IsString Builder where
+--     fromString = stringUtf8
+--
+-- -- | UTF-8 encode a 'String'.
+-- {-# INLINE stringUtf8 #-}
+-- stringUtf8 :: String -> Builder
+-- stringUtf8 = P.primMapListBounded P.charUtf8
+
+-- instance IsString Blaze.Builder where
+--   fromString                 =  Blaze.fromString
 
